3397bb41f2c152f36ac1423f01fe1d59b1195529
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2025-01-18 17:28:35 -0500

    first and second base page

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-import requests
+
+# import requests
 
 app = Flask(__name__)
 
+
 # Home route
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/solar-visualizations")
+def visualize():
+    print("GET")
+    return render_template("./solar-visualizations.html", uploadErrors=[])
